a0898ccae9eca9512f79665baedf0cc5eca1ab0e
Nadiasharmin017 <nadiasharmin017@gmail.com>
2023-08-21 14:23:15 +0600

    first commit

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 
-/*function getInnerText(id){
+function getInnerText(id){
      const productId = document.getElementById(id);
      const productElement = productId.innerText;
      return productElement;
-}*/
+}
 
 
 const cardId = "card-one";
+const productName = "product-one";
 document.getElementById(cardId).addEventListener("click", function() {
      
-    const productName = "product-one"
-    const product = getInnerText("product-one");
-     console.log("clicked the product");
+    const product = getInnerText(productName);
+    
     
  });
